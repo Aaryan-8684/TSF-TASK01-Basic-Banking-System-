f2e97e2bd74f1cec087f115ae8f7048f28d85ac9
Aryan Gupta <71130054+Aaryan-8684@users.noreply.github.com>
2021-10-20 19:53:44 +0530

    Add files via upload
    First Commit v1.0

---
+++ sparks foundation/connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$username = "epiz_30116994";
+$password="3aXuT7W69RswU4J";
+$server ="sql201.epizy.com";
+$db = "epiz_30116994_banking_db";
+
+
+$con = mysqli_connect($server,$username,$password,$db) or die("Couldn't connect");
+
+?>
---
+++ sparks foundation/viewcustomers.php
@@ -0,0 +1,197 @@
+<html>
+<head>
+<title></title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;400&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
+
+.navbar
+{
+    width:100%;
+    white-space:nowrap;
+    background-color: #093028;
+}
+.main_div
+{
+    width:100%;
+    height:100vh;
+ 
+}
+*{
+    margin:0;
+    padding:0;
+   box-sizing:border-box;
+}
+.display_table
+{
+    width:100vw;
+    height:100vh;
+    display:flex;
+    flex-direction:column;
+    justify-content: center;
+    text-align:center;
+}
+.center_div
+{
+    width:100%;
+    height:80vh;
+    background: #093028;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #237A57, #093028);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #237A57, #093028); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+h1
+{
+    font-size:28px;
+    color:#000;
+    text-align:center;
+    margin-top:-20px;
+    margin-bottom:20px;
+}
+table
+{
+    border-collapse:collapse;
+    background-color:black;
+    box-shadow:0 10px 20px 0 rgba(0,0,0,0.03);
+    border-radius: 10px;
+    border-collapse:collapse;
+    table-layout:fixed;
+    /* opacity:0.7; */
+    color:#F7CC1A;
+    font-weight:bold;
+    margin:auto;
+    
+}
+th,td
+{
+  border:1px solid #f2f2f2;
+   padding:8px 30px;
+  text-align:center;
+  /* opacity:0.9; */
+  color: #999 ; 
+}
+th{
+    text-transform:uppercase;
+    font-weight:500;
+}
+td
+{
+    font-size:13px;
+}
+
+</style>
+</head>
+<body>
+<div class="main_div">
+ 
+     <div class="navbar navbar-expand-md">
+   
+      <a href="#" class="navbar-brand font-weight-bold text-white text-center">TSF BANK</a>
+      <button class="navbar-toggler text-white " type="button" data-toggle="collapse" data-target="#collapsenavbar">
+      <span class="navbar-toggler-icon" style="background:white;"></span>
+      </button>
+     
+       <div class="collapse navbar-collapse text-center" id="collapsenavbar">
+          <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                  <a href="index.php" class="nav-link text-white">ABOUT</a></li>
+              <li class="nav-item">
+                  <a href="viewcustomers.php" class="nav-link text-white">VIEW CUSTOMER</a></li>
+              <li class="nav-item">
+                  <a href="viewcustomers.php" class="nav-link text-white"></a></li>
+              <li class="nav-item">
+                  <a href="index.php" class="nav-link text-white">CONTACT</a></li>    
+               </ul>
+        </div>
+     </div>
+
+      
+           
+          <div class="display_table">
+                 <h1>Customer Details</h1>
+                 <div class="center_div">
+               <div class="table-responsive">
+                    <table>
+                    <thead>
+                     <tr>
+                     <th>ID</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                       <th>Amount</th>
+                    
+                      <th colspan="2">operation</th>
+                    </tr>
+                    </thead>
+                   <tbody>
+                  </div>
+          <?php
+          include 'connection.php';
+          $selectquery = "select * from customer";
+          $query = mysqli_query($con,$selectquery);
+          $numofrows = mysqli_num_rows($query);
+
+           while($res = mysqli_fetch_array($query))
+          {
+            ?>
+               <tr>
+               <td><?php  echo $res['ID']; ?></td>
+               <td><?php echo $res['Name']; ?></td>
+               <td><?php echo $res['Email']; ?></td>
+               <td><?php echo $res['Amount']; ?></td>
+              <td><a href="transfermoney.php?idtransfer=<?php  echo $res['ID']; ?>" >Transfer</a></td>
+               </tr>
+             <?php
+          }
+           ?>
+
+
+</tbody>
+</table>
+</div>
+
+</div>
+
+</div>
+ </div>
+
+</body>
+</html>
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
